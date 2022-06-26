87daf52251df983da3721702c3c3e33fa25ee765
Kaan Enes Çiftçi <kaanenesciftci@gmail.com>
2022-06-26 15:30:14 +0300

    Challenge 1 is complated

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public float turnSpeed;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed* Input.GetAxis("Vertical"));
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * Input.GetAxis("Horizontal"));
+    }
+}
